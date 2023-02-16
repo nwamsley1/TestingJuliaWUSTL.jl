07d3e2719d75f6518d692e4c74e53178a6b077ff
nwamsley1 <n.t.wamsley@wustl.edu>
2023-02-16 16:52:20 -0600

    added tests for interval part 3

--- test/interval.jl
+++ test/interval.jl
@@ -4,6 +4,7 @@ using Test
     #Tests for part1
     custom_interval_a = Interval(1, 63)
     custom_interval_b = Interval(1.0, 62.56)
+    custom_interval_c = Interval(62.56, 62.56)
 
     @test minimum(custom_interval_a) == 1
     @test maximum(custom_interval_a) == 63
@@ -17,4 +18,9 @@ using Test
     @test (10 ∈ custom_interval_b) == true
     @test (10.0 ∈ custom_interval_b) == true
     @test (100.0 ∈ custom_interval_b) == false
+
+    #Tests for part3
+    @test (isempty(custom_interval_c)) == true
+    @test (isempty(custom_interval_a)) == false
+    @test (isempty(custom_interval_b)) == false
 end

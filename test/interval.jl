6f68149a22e4c93851b2f6a6568cdd2a869ddd46
nwamsley1 <n.t.wamsley@wustl.edu>
2023-02-16 16:29:45 -0600

    added tests for interval part 1

---
+++ test/interval.jl
@@ -0,0 +1,11 @@
+using TestingJuliaWUSTL
+using Test
+@testset "interval.jl" begin
+    custom_interval_a = Interval(1, 63)
+    custom_interval_b = Interval(1.0, 62.56)
+    
+    @test minimum(custom_interval_a) == 1
+    @test minimum(custom_interval_a) == 63
+    @test minimum(custom_interval_b) == 1.0
+    @test minimum(custom_interval_b) == 62.56
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,2 @@
 include("graph.jl")
+include("interval.jl")
